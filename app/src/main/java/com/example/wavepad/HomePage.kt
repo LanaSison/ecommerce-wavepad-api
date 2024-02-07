e4fbba30370034adae246f554490a2aa3c1f8c54
Albert Tirao <alli.tirao.up@phinmaed.com>
2024-02-07 16:16:07 +0800

    Adding Bottom Navigation, Home, Library, and Account

--- app/src/main/java/com/example/wavepad/HomePage.kt
+++ app/src/main/java/com/example/wavepad/HomePage.kt
@@ -1,11 +1,45 @@
 package com.example.wavepad
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HomePage: AppCompatActivity() {
+class HomePage : AppCompatActivity() {
+
+    private lateinit var bottom_Nav: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_page)
+
+        bottom_Nav = findViewById(R.id.bottomNavigationView)
+
+        bottom_Nav.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.Home -> {
+                    val intent = Intent(this, HomePage::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.Library -> {
+                    val intent = Intent(this, LibraryPage::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.Account -> {
+                    val intent = Intent(this, AccountPage::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
