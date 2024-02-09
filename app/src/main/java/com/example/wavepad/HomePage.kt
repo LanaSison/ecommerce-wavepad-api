d0babc8dfc9d2e50a4adca917b7eea2b6e32ee8d
Albert Tirao <alli.tirao.up@phinmaed.com>
2024-02-09 18:55:43 +0800

    Add drawer, search, cart, menu settings and optimize the home page fucntionallity

--- app/src/main/java/com/example/wavepad/HomePage.kt
+++ app/src/main/java/com/example/wavepad/HomePage.kt
@@ -2,44 +2,40 @@ package com.example.wavepad
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
-import androidx.appcompat.widget.Toolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomePage : AppCompatActivity() {
 
-    private lateinit var bottom_Nav: BottomNavigationView
+    private lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_page)
 
-        bottom_Nav = findViewById(R.id.bottomNavigationView)
+        bottomNav = findViewById(R.id.bottomNavigationView)
 
-        bottom_Nav.setOnItemSelectedListener { menuItem ->
+        bottomNav.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.Home -> {
-                    val intent = Intent(this, HomePage::class.java)
-                    startActivity(intent)
+                R.id.home -> {
+                    startNewActivity(HomePage::class.java)
                     true
                 }
-
-                R.id.Library -> {
-                    val intent = Intent(this, LibraryPage::class.java)
-                    startActivity(intent)
+                R.id.voucher -> {
+                    startNewActivity(LibraryPage::class.java)
                     true
                 }
-
-                R.id.Account -> {
-                    val intent = Intent(this, AccountPage::class.java)
-                    startActivity(intent)
+                R.id.account -> {
+                    startNewActivity(AccountPage::class.java)
                     true
                 }
-
                 else -> false
             }
         }
+    }
+
+    private fun startNewActivity(activityClass: Class<*>) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
     }
 }
